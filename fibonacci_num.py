2cdf9e41535bf33cbb9088bc499b24fbe7a10fbc
jeevita <bhupati.jeevita@gmail.com>
2026-02-03 22:16:07 +0530

    Day_11

--- fibonacci_num.py
+++ fibonacci_num.py
@@ -1,7 +1,8 @@
-class Solution:
-    def fib(self, n: int) -> int:
-        if n <= 0:
-            return 0
-        if n == 1:
-            return 1
-        return self.fib(n - 1) + self.fib(n - 2)
+def fib(n):
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+print(fib(int(input())))
